ae335195e107d088a9f9009de730ed4f4a296563
richdonny <108276980+richdonny@users.noreply.github.com>
2026-01-19 20:07:12 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -21,10 +21,10 @@ if __name__ == '__main__':
     if cookies[0] != "":
 
         check_in_url = "https://glados.cloud/console/checkin"        # 签到地址
-        status_url = "https://glados.space/api/user/status"          # 查看账户状态
+        status_url = "https://glados.cloud/api/user/status"          # 查看账户状态
 
-        referer = 'https://glados.space/console/checkin'
-        origin = "https://glados.space"
+        referer = 'https://glados.cloud/console/checkin'
+        origin = "https://glados.cloud"
         useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36"
         payload = {
             'token': 'glados.one'
@@ -95,6 +95,7 @@ if __name__ == '__main__':
     else:
         pushdeer = PushDeer(pushkey=sckey) 
         pushdeer.send_text(title, desp=context)
+
 
 
 
